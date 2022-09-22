6e802829f8d612c188806ca13a1c49af93356436
Jost Schulte <jost.schulte@protonmail.com>
2022-09-22 14:40:39 -0500

    try with nix tarball

--- holochain.nix
+++ holochain.nix
@@ -2,7 +2,7 @@ let
   pname = "holochain";
   version = "0.0.162";
   rust-overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
-  nixpkgs = import <nixpkgs> { overlays = [ rust-overlay ]; };
+  nixpkgs = import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz") { overlays = [ rust-overlay ]; };
 in
 nixpkgs.stdenv.mkDerivation
 {
