918ceaff1fad7d7da701089e9074d3b23b1aa84a
Jost Schulte <jost.schulte@protonmail.com>
2022-09-23 20:46:03 -0500

    build 152 derivations

--- holochain.nix
+++ holochain.nix
@@ -1,6 +1,6 @@
 let
   pname = "holochain";
-  version = "0.0.146";
+  version = "0.0.152";
   rust-overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
   nixpkgs = import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/e14f9fb57315f0d4abde222364f19f88c77d2b79.tar.gz") { overlays = [ rust-overlay ]; };
 in
@@ -19,7 +19,7 @@ nixpkgs.stdenv.mkDerivation
   src = builtins.fetchGit
     {
       url = https://github.com/holochain/holochain;
-      rev = "377be199339179d02502a817ad8babe23f89a7bc";
+      rev = "30f6accd88faa0c2946d63a77d0ba83f82df3a91";
     };
 
   buildPhase = ''
