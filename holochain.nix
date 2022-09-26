849a9add6ced01ad5498917342a2d7d9c1bd507d
Jost Schulte <jost.schulte@protonmail.com>
2022-09-25 21:48:00 -0500

    0.0.158 with rustPlatform

--- holochain.nix
+++ holochain.nix
@@ -4,35 +4,24 @@ let
   rust-overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
   nixpkgs = import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/e14f9fb57315f0d4abde222364f19f88c77d2b79.tar.gz") { overlays = [ rust-overlay ]; };
 in
-nixpkgs.stdenv.mkDerivation
-{
+nixpkgs.rustPlatform.buildRustPackage rec {
   inherit pname version;
 
-  buildInputs = [
-    (nixpkgs.rust-bin.stable."1.60.0".default.override {
+  nativeBuildInputs = [
+    (nixpkgs.rust-bin.stable.latest.default.override {
       extensions = [ "rust-src" ];
       targets = [ "wasm32-unknown-unknown" ];
     })
-    nixpkgs.darwin.apple_sdk.frameworks.AppKit
   ];
 
-  src = builtins.fetchGit
-    {
-      url = https://github.com/holochain/holochain;
-      rev = "0cd777c789ee13ca655f9bffbd7fe11b49e47824";
-    };
+  buildInputs = [ nixpkgs.darwin.apple_sdk.frameworks.AppKit ];
 
-  buildPhase = ''
-    CARGO_HOME=.cargo cargo build --release --bin holochain --bin hc
-  '';
+  src = builtins.fetchGit {
+    url = "https://github.com/holochain/holochain";
+    rev = "0cd777c789ee13ca655f9bffbd7fe11b49e47824";
+  };
 
-  installPhase = ''
-    mkdir -p $out/bin
-    mv target/release/holochain $out/bin
-    mv target/release/hc $out/bin
-  '';
+  cargoSha256 = "hAvMZccSgqjdGphPQ4CS4G12MduKclZPcG70RxBL6LU=";
 
-  installCheckPhase = ''
-    export PATH="$out/bin:$PATH"
-  '';
+  doCheck = false;
 }
