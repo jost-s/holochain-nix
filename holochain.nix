c4f37e101f2cada2f199c70d14cedf59bb41335a
Jost Schulte <jost.schulte@protonmail.com>
2022-11-10 09:18:17 -0600

    build 0.0.170 again

--- holochain.nix
+++ holochain.nix
@@ -1,6 +1,6 @@
 let
   pname = "holochain";
-  version = "signed-zome-calls-2";
+  version = "0.0.170";
   rust-overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
   nixpkgs = import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/e14f9fb57315f0d4abde222364f19f88c77d2b79.tar.gz") { overlays = [ rust-overlay ]; };
 in
@@ -18,11 +18,10 @@ nixpkgs.rustPlatform.buildRustPackage rec {
 
   src = builtins.fetchGit {
     url = "https://github.com/holochain/holochain";
-    rev = "d2bfcc7d8345643184ea8dbbe2ee3028a448b93a";
-    ref = "fix/signed-zome-call-resolve-conflicts";
+    rev = "e9364427f6ebe1a7f3a8d75d252fd8445c039f2a";
   };
 
-  cargoSha256 = "SRKvXdxZkgxYH2sbZNBbM+tf9jgEm0Xg/GZfGj7rPZ8=";
+  cargoSha256 = "xgC3oAU9qKaTEEsl4a6m7CuFDFsdTutP3Ov6TkTSVEI=";
 
   doCheck = false;
 }
