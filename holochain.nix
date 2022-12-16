54c45ba9edfe2b0d5afcc1adf1dcc1ed7f2911b5
Jost Schulte <jost.schulte@protonmail.com>
2022-12-16 14:05:29 -0600

    build 0.1.0-beta-rc.1

--- holochain.nix
+++ holochain.nix
@@ -1,6 +1,6 @@
 let
   pname = "holochain";
-  version = "0.1.0-beta-rc.0";
+  version = "0.1.0-beta-rc.1";
   rust-overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
   nixpkgs = import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/e14f9fb57315f0d4abde222364f19f88c77d2b79.tar.gz") { overlays = [ rust-overlay ]; };
 in
@@ -18,10 +18,10 @@ nixpkgs.rustPlatform.buildRustPackage rec {
 
   src = builtins.fetchGit {
     url = "https://github.com/holochain/holochain";
-    rev = "6adca3a4ab0121754be3af019c75a5222c17779a";
+    rev = "a28eff8dbd688f59e2d16347185040336a0177f0";
   };
 
-  cargoSha256 = "I5q4w+33Hzk7ZqKtVGGvaiCyqM430fCiW8p409Hbmv4=";
+  cargoSha256 = "Q1CzDlyB6TeD8ixKnuKk9wfZ3YJM967lzIinqvPL8pE=";
 
   doCheck = false;
 }
